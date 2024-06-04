b281239c07b716e7124902f99eee83abed8612a6
shoh4n <maffin.killer007@gmail.com>
2024-06-04 17:51:37 +0600

    Get Triangle Base And Height Using Event And Function

--- scripts/triangle.js
+++ scripts/triangle.js
@@ -1,3 +1,11 @@
 function calculateTriangleArea(){
-    console.log('doing triangle area calculation');
+    const triangleBaseInput = document.getElementById('triangleBase');
+    const triangleBaseValue = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseValue);
+    // console.log(base);
+    // get triangle height 
+    const triangleHeightInput = document.getElementById('triangleHeight');
+    const triangleHeightValue = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightValue);
+    console.log(height);
 }
