dc72a2a5c98feca090b016bb443099ebfec578f7
Shohan Sharoar <maffin.killer007@gmail.com>
2024-06-05 12:09:57 +0600

    Create A Reusable Function For Similar Task

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,21 @@
 function calculateRectangleArea(){
-    console.log('connected');
+    const length = getInputValueById("lengthInput");
+    const width = getInputValueById('widthInput');
+
+    const area = length * width;
+
+    setInnerTex('rectangleAreaSpan', area);
+}
+
+function getInputValueById(inputFieldID){
+    // grave the input field value
+    const inputField = document.getElementById(inputFieldID);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    return inputValue;
+}
+
+function setInnerTex( id, area ){
+    const shapeAreaSpan = document.getElementById(id);
+    shapeAreaSpan.innerText = area;
 }
